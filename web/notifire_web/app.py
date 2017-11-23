5b40783320afd7d112d511a1f4214070c8d26959
ab24v07 <ash.booth@soton.ac.uk>
2017-11-23 18:23:13 +0000

    added barebones for auth, login working, need to add logout and more auth decorators for APIs. Also need to tie in to DB Users

--- web/notifire_web/app.py
+++ web/notifire_web/app.py
@@ -7,7 +7,7 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, authenticated
 from tornado.websocket import WebSocketHandler
-# from tornado.escape import xhtml_escape
+from tornado.escape import xhtml_escape, json_encode, url_escape
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
@@ -27,22 +27,39 @@ class BaseHandler(RequestHandler):
         return self.get_secure_cookie("user")
 
 
-# class AuthLoginHandler(BaseHandler):
-#     def get(self):
-#         try:
-#             errormessage = self.get_argument("error")
-#         except:
-#             errormessage = ""
-#         self.render("login.html", errormessage = errormessage)
-#
-#     def check_permission(self, username, password):
-#         if username == "admin" and password == "admin":
-#             return True
-#         return False
+class AuthLoginHandler(BaseHandler):
+    def get(self):
+        try:
+            error_message = self.get_argument("error")
+        except:
+            error_message = ""
+        self.render("login.html", errormessage = error_message)
+
+    def check_permission(self, username, password):
+        if username == "admin" and password == "admin":
+            return True
+        return False
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", json_encode(user))
+        else:
+            self.clear_cookie("user")
+
+    def post(self):
+        username = self.get_argument("username", "")
+        password = self.get_argument("password", "")
+        auth = self.check_permission(username, password)
+        if auth:
+            self.set_current_user(username)
+            self.redirect(self.get_argument("next", u"/"))
+        else:
+            error_message = u"?error={}".format(url_escape("Login incorrect"))
+            self.redirect(u"/auth/login/" + error_message)
 
 
 class HelloHandler(RequestHandler):
-    # @authenticated
+    @authenticated
     def get(self):
         try:
             visits = redis.incr("counter")
@@ -87,14 +104,14 @@ def make_app():
     handlers = [
         (r'/hello', HelloHandler),
         (r'/', IndexPageHandler),
-        # (r"/auth/login/", AuthLoginHandler),
+        (r"/auth/login/", AuthLoginHandler),
         (r'/websocket', MyWsHandler),
         (r'/users', UserHandler)
     ]
     settings = {
         'template_path': TEMPLATE_PATH,
-        # "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-        # "login_url": "/auth/login/"
+        "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        "login_url": "/auth/login/"
     }
     return Application(
         handlers,
